ed3e92db50c8ae306a86bf5d4be11ccb682f7152
NitroRiser <jonathanfariastech@gmail.com>
2022-09-04 11:35:07 -0300

    Order, Instant, ISO 8601 - PART 2

--- course/src/main/java/com/jtech/course/entities/Order.java
+++ course/src/main/java/com/jtech/course/entities/Order.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name ="tb_order")
 public class Order implements Serializable {
