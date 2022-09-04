366eed2886bb4070d88294e73e4b70cf9fc43f26
NitroRiser <jonathanfariastech@gmail.com>
2022-09-04 15:54:18 -0300

    Category

---
+++ course/src/main/java/com/jtech/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.jtech.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jtech.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+	
+}
